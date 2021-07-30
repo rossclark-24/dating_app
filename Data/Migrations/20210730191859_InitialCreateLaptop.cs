227a2ee9a025eaa4fe39e4231b8bc31575570bfd
Ross Clark <ross.clark@uky.edu>
2021-07-30 15:25:52 -0400

    added db to laptop local

---
+++ Data/Migrations/20210730191859_InitialCreateLaptop.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace API.Data.Migrations
+{
+    public partial class InitialCreateLaptop : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
